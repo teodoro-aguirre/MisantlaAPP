68f505d7b748742cb9955a022910b32f2abe7223
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-11-14 22:14:05 -0600

    Check y request permission

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -8,6 +8,7 @@ const Stack = createStackNavigator();
 export const Navigator = () => {
   return (
     <Stack.Navigator
+        initialRouteName = "PermissionsScreen"
         screenOptions={{
             headerShown: false,
             cardStyle: {
