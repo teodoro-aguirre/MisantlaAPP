16b5f9573d5db8eae888c365077a137e22ffc937
Angel Rodriguez <rodriguezaguirreangelt@gmail.com>
2021-11-15 00:59:21 -0600

    Detectar que pantalla mostrar automaticamente

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MapScreen } from '../pages/MapScreen';
 import { PermissionsScreen } from '../pages/PermissionsScreen';
+import { PermissionsContext } from '../context/PermissionsContext';
+import { LoadingScreen } from '../pages/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
+
+  const { permissions } = useContext( PermissionsContext )
+
+  if(permissions.locationStatus === 'unavailable'){
+    return <LoadingScreen /> ;
+  }
+
   return (
     <Stack.Navigator
         initialRouteName = "PermissionsScreen"
@@ -16,8 +25,13 @@ export const Navigator = () => {
             }
         }}
     >
-      <Stack.Screen name="MapScreen" component={MapScreen} />
-      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+
+      {
+        (permissions.locationStatus === 'granted')
+        ? <Stack.Screen name="MapScreen" component={MapScreen} /> 
+        : <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+
+      }
     </Stack.Navigator>
   );
 }
