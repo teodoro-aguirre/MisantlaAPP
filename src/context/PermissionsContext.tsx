faea01ba9ff7966798e2e7b3f1802f1fabca0ba0
Angel Rodriguez <rodriguezaguirreangelt@gmail.com>
2021-11-15 02:34:15 -0600

    Fix: Loading infinito en ocasiones

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -23,7 +23,11 @@ export const PermissionsProvider = ({ children }: any) => {
     const [permissions, setPermissions] = useState( permissionInitState );
 
     useEffect(() => {
+
+        checkLocationPermission();
+
         AppState.addEventListener('change', state => {
+            
             if( state !== 'active') return;
 
             checkLocationPermission();
