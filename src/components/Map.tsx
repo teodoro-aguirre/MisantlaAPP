b5d3683c081e9359374a79a9a6077105948f0014
Angel Rodriguez <rodriguezaguirreangelt@gmail.com>
2021-11-15 02:46:34 -0600

    Marcadores y componentes Mapa re-utilizable

---
+++ src/components/Map.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import MapView, { Marker } from 'react-native-maps'
+
+export const Map = () => {
+    return (
+        <>
+            <MapView
+                style={{flex: 1}}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+            <Marker
+                image={require('../assets/custom-marker.png')}
+                coordinate={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                }}
+                title="Esto es un titulo"
+                description="Descripción del marcador"
+            />  
+            </MapView> 
+        </>
+    )
+}
--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import MapView from 'react-native-maps';
+import { View } from 'react-native'
+import { Map } from '../components/Map'
 
 export const MapScreen = () => {
     return (
         <View style={{ flex: 1, }}>
-            <MapView
-                style={{flex: 1}}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+            
+            <Map />
+            
         </View>
     )
 }
