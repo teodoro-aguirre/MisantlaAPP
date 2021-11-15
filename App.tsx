a250365fcc75b9c118546df2fe218f641f71ae3c
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-11-14 22:48:36 -0600

    Permission Provider - ContextAPI

--- App.tsx
+++ App.tsx
@@ -3,11 +3,24 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { Navigator } from './src/navigator/Navigator';
+import { PermissionsProvider } from './src/context/PermissionsContext';
+
+
+const AppState = ({children}: any) => {
+  return (
+    <PermissionsProvider>
+      { children }
+    </PermissionsProvider>
+  )
+}
+
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator/>
+      <AppState>
+        <Navigator/>
+      </AppState>
     </NavigationContainer>
   )
 }
